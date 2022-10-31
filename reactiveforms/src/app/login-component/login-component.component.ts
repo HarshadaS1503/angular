29c2d8b573e0b671da9706b575c02e93638ca3b6
Harshadas15 <harshadasawant_15@live.com>
2022-10-31 19:14:08 +0530

    second

--- reactiveforms/src/app/login-component/login-component.component.ts
+++ reactiveforms/src/app/login-component/login-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponentComponent implements OnInit {
 
+  signInForm:FormGroup;
+  regExp=  new RegExp('[^.,a-zA-Z0-9]');
   constructor() { }
 
   ngOnInit(): void {
+    this.signInForm = new FormGroup({
+       'username': new FormControl('',[Validators.required,Validators.maxLength(10),Validators.pattern(this.regExp)]),
+       'password': new FormControl(null,[Validators.required,Validators.maxLength(10),Validators.pattern(this.regExp)])
+    });
   }
 
+   log()
+   {
+    console.log(this.signInForm.get('username').value);
+    
+    console.log(this.signInForm.get('password').value)
+   }
+
+  OnSubmit()
+  {
+    
+  }
+
+  checkValue(control:FormControl):{[s:string]:boolean}
+  {
+    if(!this.regExp.test(control.value))
+    {
+      return { result :true};
+    }
+    return null;
+    
+
+  }
 }
